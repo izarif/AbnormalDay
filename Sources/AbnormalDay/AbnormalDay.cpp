2363346ac03d06ff1abc8f91cfbd86d6257024c8
iZarif <aydar.js@gmail.com>
2024-12-24 15:02:31 +0400

    dont print display mode info

--- Sources/AbnormalDay/AbnormalDay.cpp
+++ Sources/AbnormalDay/AbnormalDay.cpp
@@ -702,9 +702,6 @@ void DoGame(void)
       bMenuRendering = DoMenu(pdp);
     }
 
-    // print display mode info if needed
-    PrintDisplayModeInfo();
-
     // render console
     _pGame->ConsoleRender(pdp);
 
