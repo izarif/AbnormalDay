13e86b9cdade1bb37bc25f146ba38c618ae2b0f3
iZarif <aydar.js@gmail.com>
2025-05-06 18:16:42 +0400

    dont print display info

--- Sources/SeriousSam/SeriousSam.cpp
+++ Sources/SeriousSam/SeriousSam.cpp
@@ -877,9 +877,6 @@ void DoGame(void)
       bMenuRendering = DoMenu(pdp);
     }
 
-    // print display mode info if needed
-    PrintDisplayModeInfo();
-
     // render console
     _pGame->ConsoleRender(pdp);
 
